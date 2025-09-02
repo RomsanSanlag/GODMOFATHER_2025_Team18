feaa6c135ecd4bf7bc28f418c5767a89380a6fc4
Romsan <112938375+RomsanSanlag@users.noreply.github.com>
2025-09-02 16:56:28 +0200

    [ADD] prefab objects

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{    
+    [SerializeField] List<GameObject> _spawnObjects = new List<GameObject>();
+    
+    void Update()
+    {
+        
+    }
+}
