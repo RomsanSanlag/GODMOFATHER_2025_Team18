1a594264454b1244893e7e53bb7e2d3e0d8d8683
Romsan <112938375+RomsanSanlag@users.noreply.github.com>
2025-09-04 15:18:54 +0200

    [ADD] Scriptable Objects

---
+++ Assets/ScriptableObjects/Objects.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Objects : ScriptableObject
+{
+    public string itemName;
+    public int value;
+    public GameObject prefab;
+    public int index; // index dans la liste pour le spawn sur le paddle
+}
